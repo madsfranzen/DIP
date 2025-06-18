c0920ef03fa932c6c508eb1e39322d845669de4a
Mads Franzen <madsfranzen@live.dk>
2025-06-18 14:40:03 +0200

    SPM8

--- EksamenPrep/EXAM/main.js
+++ EksamenPrep/EXAM/main.js
@@ -1,3 +1,46 @@
 console.clear();
-console.log('main.js\n');
+// console.log('main.js\n');
 
+function randomNumber(ceiling) {
+	let numberOfCalls = 0;
+	return {
+		generate: function generate() {
+			numberOfCalls++;
+			return Math.floor(Math.random() * ceiling);
+		},
+		getNumberOfCalls: () => numberOfCalls,
+	};
+}
+
+class Person {
+	age;
+	height;
+
+	constructor(age, height) {
+		this.age = age;
+		this.height = height;
+	}
+}
+
+const ageGenerator = randomNumber(100);
+const heightGenerator = randomNumber(200);
+
+heightGenerator.generate();
+
+function createRandomPersons(ageGenerator, heightGenerator) {
+	const personArray = [];
+	for (let i = 0; i < 20; i++) {
+		const person = new Person(
+			ageGenerator.generate(),
+			heightGenerator.generate()
+		);
+		personArray.push(person);
+	}
+	return personArray;
+}
+
+console.log(createRandomPersons(ageGenerator, heightGenerator));
+console.log(`AgeGenerator was called ${ageGenerator.getNumberOfCalls()} times`);
+console.log(
+	`HeightGenerator was called ${heightGenerator.getNumberOfCalls()} times`
+);
